29b6cd7300b9601757cd731e5dff7f259e815ba6
cmygray <cmygray@gmail.com>
2019-06-10 12:11:04 +0900

    feat(plugin): Vuetify 컴포넌트 설치
    Signed-off-by: cmygray <cmygray@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,22 @@
 import 'reflect-metadata';
 
 import Vue from 'vue';
+import Vuetify from 'vuetify';
+
+import 'vuetify/dist/vuetify.min.css';
+
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import './plugins';
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
+  // @ts-ignore
+  vuetify: new Vuetify(),
   /* eslint-disable-next-line @typescript-eslint/explicit-function-return-type */
   render: h => h(App),
 }).$mount('#app');
