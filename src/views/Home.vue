13b9a19aa3db14430cb82a3524ad134dac1c1e20
cmygray <cmygray@gmail.com>
2019-06-12 18:10:54 +0900

    feature(Home): GENERATE 버튼을 누르면 사용자의 저장소 목록 데이터 요청
    changes:
    - Vuex action 과 submit 이벤트를 연동
    - 아이디를 입력하지 않으면 버튼을 비활성화
    - vuex-class Annotation을 사용하여 컴포넌트에 action 바인딩
    
    Signed-off-by: cmygray <cmygray@gmail.com>

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,11 +8,7 @@
         label="GitHub ID"
         placeholder="cmygray"
       />
-      <v-btn
-        :disabled="isLoading || !userId"
-        type="submit"
-        block
-        color="primary"
+      <v-btn :disabled="!userId" type="submit" block color="primary"
         >Generate</v-btn
       >
     </form>
@@ -25,28 +21,21 @@ import { Action } from 'vuex-class';
 
 @Component
 export default class Home extends Vue {
-  private userId: string = '';
-  private isLoading: boolean = false;
+  userId: string = '';
 
   @Action('fetchRepositories')
-  private fetchRepositories: (param: { userId: string }) => Promise<void>;
+  fetchRepositories: (param: { userId: string }) => Promise<void>;
 
-  private async handleSubmit() {
-    this.isLoading = true;
-    try {
-      await this.fetchRepositories({
+  async handleSubmit() {
+    await this.fetchRepositories({
+      userId: this.userId,
+    });
+    this.$router.push({
+      name: 'repos.index',
+      query: {
         userId: this.userId,
-      });
-      this.$router.push({
-        name: 'repos.index',
-        query: {
-          userId: this.userId,
-        },
-      });
-    } catch (error) {
-      console.log(error);
-    }
-    this.isLoading = false;
+      },
+    });
   }
 }
 </script>
